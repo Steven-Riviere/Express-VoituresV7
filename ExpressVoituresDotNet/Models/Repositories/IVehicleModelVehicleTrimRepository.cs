ed8319b07bad501d5d30f64d632ddf29c94a8fb0
Steven Rivière <stevensteriviere@gmail.com>
2025-10-03 10:36:51 +0200

    create relation model with trim

---
+++ ExpressVoituresDotNet/Models/Repositories/IVehicleModelVehicleTrimRepository.cs
@@ -0,0 +1,14 @@
+﻿using ExpressVoituresDotNet.Models.Entities;
+
+namespace ExpressVoituresDotNet.Models.Repositories
+{
+    public interface IVehicleModelVehicleTrimRepository
+    {
+        Task<bool> ExistsAsync(int modelId, int trimId);
+        Task AddAsync(VehicleModelVehicleTrim entity);
+        Task RemoveAsync(int modelId, int trimId);
+        Task<IEnumerable<VehicleModelVehicleTrim>> GetAllAsync();
+        Task<IEnumerable<VehicleModel>> GetModelsByTrimIdAsync(int trimId);
+        Task<IEnumerable<VehicleTrim>> GetTrimsByModelIdAsync(int modelId);
+    }
+}
---
+++ ExpressVoituresDotNet/Models/Repositories/VehicleModelVehicleTrimRepository.cs
@@ -0,0 +1,66 @@
+﻿using ExpressVoituresDotNet.Data;
+using ExpressVoituresDotNet.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpressVoituresDotNet.Models.Repositories
+{
+    public class VehicleModelVehicleTrimRepository : IVehicleModelVehicleTrimRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VehicleModelVehicleTrimRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> ExistsAsync(int modelId, int trimId)
+        {
+            return await _context.VehicleModelVehicleTrims
+                .AnyAsync(vbm => vbm.VehicleModelId == modelId && vbm.VehicleTrimId == trimId);
+        }
+
+        public async Task AddAsync(VehicleModelVehicleTrim entity)
+        {
+            _context.VehicleModelVehicleTrims.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(int modelId, int trimId)
+        {
+            var entity = await _context.VehicleModelVehicleTrims
+                .FirstOrDefaultAsync(vbm => vbm.VehicleModelId == modelId && vbm.VehicleTrimId == trimId);
+
+            if (entity != null)
+            {
+                _context.VehicleModelVehicleTrims.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<VehicleModelVehicleTrim>> GetAllAsync()
+        {
+            return await _context.VehicleModelVehicleTrims
+                .Include(vbm => vbm.VehicleModel)
+                .Include(vbm => vbm.VehicleTrim)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<VehicleModel>> GetModelsByTrimIdAsync(int trimId)
+        {
+            return await _context.VehicleModelVehicleTrims
+                .Where(vbm => vbm.VehicleTrimId == trimId)
+                .Include(vbm => vbm.VehicleModel)
+                .Select(vbm => vbm.VehicleModel)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<VehicleTrim>> GetTrimsByModelIdAsync(int modelId)
+        {
+            return await _context.VehicleModelVehicleTrims
+                .Where(vbm => vbm.VehicleModelId == modelId)
+                .Include(vbm => vbm.VehicleTrim)
+                .Select(vbm => vbm.VehicleTrim)
+                .ToListAsync();
+        }
+    }
+}
