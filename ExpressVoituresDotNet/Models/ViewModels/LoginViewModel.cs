289dc5d9ac17f4ca88350554483b907e85dd124e
Steven Rivière <stevensteriviere@gmail.com>
2025-10-03 08:12:36 +0200

    Files ViewModels

---
+++ ExpressVoituresDotNet/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpressVoituresDotNet.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+
+        [Required]
+        [EmailAddress(ErrorMessage = "L'adresse e-mail n'est pas valide.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Le mot de passe est requis.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ ExpressVoituresDotNet/Models/ViewModels/VehicleViewModel.cs
@@ -0,0 +1,81 @@
+﻿using ExpressVoituresDotNet.Models.Entities;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpressVoituresDotNet.Models.ViewModels
+{
+    public class VehicleViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Label { get; set; } = null!;
+
+        [Required]
+        public string VIN { get; set; } = null!;
+
+        [MaxLength(250, ErrorMessage = "La description ne peut pas dépasser 250 caractères.")]
+        public string? Description { get; set; }
+
+        [Range(1990, 2025, ErrorMessage = "L'année de production doit être comprise entre 1990 et 2025.")]
+        [Required]
+        public int YearOfProduction { get; set; }
+
+        [Required]
+        public int VehicleBrandId { get; set; }
+
+        [Required]
+        public int VehicleModelId { get; set; }
+
+        public int? VehicleTrimId { get; set; }
+
+        public VehicleStatus Status { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Purchase { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal? PurchasePrice { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? Sale { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal? SalePrice { get; set; }
+
+        // Info média unique
+        [BindNever]
+        public string MediaLabel { get; set; } = null!;
+        [BindNever]
+        public string MediaPath { get; set; } = null!;
+        [Required(ErrorMessage = "Une image est obligatoire.")]
+        public IFormFile MediaFile { get; set; } = null!;
+
+        // Infos liées (non bindées, juste pour affichage)
+        [BindNever]
+        public string? BrandName { get; set; }
+        [BindNever]
+        public string? ModelName { get; set; }
+        [BindNever]
+        public string? TrimName { get; set; }
+       
+        [MaxLength(250, ErrorMessage = "La description ne peut pas dépasser 250 caractères.")]
+        public string? RepairDescription { get; set; }
+       
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? RepairDate { get; set; }
+       
+        [DataType(DataType.Currency)]
+        public decimal? RepairCost { get; set; }
+
+        // Pour les dropdowns dans le formulaire
+        [BindNever]
+        public IEnumerable<SelectListItem>? VehicleBrands { get; set; }
+        [BindNever]
+        public IEnumerable<SelectListItem>? VehicleModels { get; set; }
+        [BindNever]
+        public IEnumerable<SelectListItem>? VehicleTrims { get; set; }
+        [BindNever]
+        public IEnumerable<SelectListItem>? YearsOfProduction { get; set; }
+    }
+}
