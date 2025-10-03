c3229702db7b172027fdfbc3bc97204745a60ea2
Steven Rivière <stevensteriviere@gmail.com>
2025-10-03 10:42:28 +0200

    create service for relation model with trim

---
+++ ExpressVoituresDotNet/Models/Services/IVehicleModelVehicleTrimService.cs
@@ -0,0 +1,13 @@
+﻿using ExpressVoituresDotNet.Models.Entities;
+
+namespace ExpressVoituresDotNet.Models.Services
+{
+    public interface IVehicleModelVehicleTrimService
+    {
+        Task<bool> ExistsAsync(int modelId, int trimId);
+        Task AddAsync(int modelId, int trimId);
+        Task RemoveAsync(int modelId, int trimId);
+        Task<IEnumerable<VehicleModel>> GetModelsByTrimIdAsync(int trimId);
+        Task<IEnumerable<VehicleTrim>> GetTrimsByModelIdAsync(int modelId);
+    }
+}
---
+++ ExpressVoituresDotNet/Models/Services/VehicleModelVehicleTrimService.cs
@@ -0,0 +1,48 @@
+﻿using ExpressVoituresDotNet.Models.Entities;
+using ExpressVoituresDotNet.Models.Repositories;
+
+namespace ExpressVoituresDotNet.Models.Services
+{
+    public class VehicleModelVehicleTrimService : IVehicleModelVehicleTrimService
+    {
+        private readonly VehicleModelVehicleTrimRepository _repository;
+
+        public VehicleModelVehicleTrimService(VehicleModelVehicleTrimRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> ExistsAsync(int modelId, int trimId)
+        {
+            return await _repository.ExistsAsync(modelId, trimId);
+        }
+
+        public async Task AddAsync(int modelId, int trimId)
+        {
+            if (!await ExistsAsync(modelId, trimId))
+            {
+                var entity = new VehicleModelVehicleTrim
+                {
+                    VehicleModelId = modelId,
+                    VehicleTrimId = trimId
+                };
+                await _repository.AddAsync(entity);
+            }
+        }
+
+        public async Task RemoveAsync(int modelId, int trimId)
+        {
+            await _repository.RemoveAsync(modelId, trimId);
+        }
+
+        public async Task<IEnumerable<VehicleModel>> GetModelsByTrimIdAsync(int trimId)
+        {
+            return await _repository.GetModelsByTrimIdAsync(trimId);
+        }
+
+        public async Task<IEnumerable<VehicleTrim>> GetTrimsByModelIdAsync(int modelId)
+        {
+            return await _repository.GetTrimsByModelIdAsync(modelId);
+        }
+    }
+}
